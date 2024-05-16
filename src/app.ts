9c7e35ff355090be6ab6df64da301662e8e04775
Leslie Chou <chouyuchien@gmail.com>
2024-05-16 15:27:30 -0700

    added autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
+function autobind (
+    // target: any,
+    // methodName: string,
+    _ : any,
+    _2: string,
+    descriptor: PropertyDescriptor //Property Descriptor is a built-in type in TypeScript
+) {
+
+    // console.log("descriptor: ", descriptor); //descriptor is an object that contains the configuration of the method (value, writable, enumerable, configurable
+    const originalMethod = descriptor.value;
+    // console.log("originalMethod: ", originalMethod);
+    const adjDescriptor: PropertyDescriptor = { //create a new descriptor object
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -23,13 +45,41 @@ class ProjectInput {
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input, please try again');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value); //'this' keyword is not accessible here if it is not binded to the class
+        // console.log(this.titleInputElement.value); //'this' keyword is not accessible here if it is not bind to the class
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+
+        this.clearInputs();
     };
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this)); //bind the this keyword to the class
+        this.element.addEventListener('submit', this.submitHandler.bind(this)); //bind the this keyword to the class instead of the event target
     };
 
     private attach() {
